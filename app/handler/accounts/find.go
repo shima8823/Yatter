c949ef0365ae2522109ff5817edbb105b085555b
shima8823 <taiyaki1275@gmail.com>
2023-08-18 12:02:27 +0900

    intial commit

---
+++ app/handler/accounts/find.go
@@ -0,0 +1,28 @@
+package accounts
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"net/http"
+	"yatter-backend-go/app/handler/httperror"
+)
+
+// Handler request for `GET /v1/accounts/username`
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	username := chi.URLParam(r, "username")
+
+	w.Header().Set("Content-Type", "application/json")
+	if objAccount, err := h.app.Dao.Account().FindByUsername(ctx, username); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	} else if objAccount != nil {
+		if err := json.NewEncoder(w).Encode(objAccount); err != nil {
+			httperror.InternalServerError(w, err)
+			return
+		}
+	} else {
+		httperror.NotFound(w, username)
+		return
+	}
+}
