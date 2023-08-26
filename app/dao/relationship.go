ed6bb408d4344cc3604b0a5d617e6753971f107a
shima8823 <taiyaki1275@gmail.com>
2023-08-26 19:40:09 +0900

    feat: Create, Findの実装

--- app/dao/relationship.go
+++ app/dao/relationship.go
@@ -2,6 +2,9 @@ package dao
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/domain/repository"
 
@@ -21,5 +24,21 @@ func NewRelationship(db *sqlx.DB) repository.Relationship {
 }
 
 func (r *relationship) CreateFollowing(ctx context.Context, followingID object.AccountID, followerID object.AccountID) error {
+	if _, err := r.db.ExecContext(ctx, "insert into relationship (following_id, follower_id) values (?, ?)", followingID, followerID); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (r *relationship) FindAccountByUsername(ctx context.Context, username string) (*object.Account, error) {
+	entity := new(object.Account)
+	err := r.db.QueryRowxContext(ctx, "select * from account where username = ?", username).StructScan(entity)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("%w", err)
+	}
+	return entity, nil
 }
