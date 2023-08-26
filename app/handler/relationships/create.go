d1eddda2c3dbd2cb51e78c6efa53a016d5502ba5
shima8823 <taiyaki1275@gmail.com>
2023-08-26 19:39:22 +0900

    feat: createHandlerの実装

--- app/handler/relationships/create.go
+++ app/handler/relationships/create.go
@@ -1,9 +1,55 @@
 package relationships
 
 import (
+	"encoding/json"
 	"net/http"
+	"yatter-backend-go/app/domain/object"
+	"yatter-backend-go/app/handler/auth"
+	"yatter-backend-go/app/handler/httperror"
+	"yatter-backend-go/app/handler/request"
 )
+
+type AddRequest struct {
+	followingID string
+	followerID  string
+}
 
 // Handler request for `GET /v1/accounts/{username}/follow`
 func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
+	followingAccount := auth.AccountOf(r)
+	if followingAccount == nil {
+		httperror.Error(w, http.StatusUnauthorized)
+		return
+	}
+	var followerUsername string
+	var followerAccount *object.Account
+	var err error
+
+	followerUsername, err = request.UsernameOf(r)
+	if err != nil {
+		httperror.BadRequest(w, err)
+		return
+	}
+	ctx := r.Context()
+
+	repo := h.app.Dao.Relationship()
+	followerAccount, err = repo.FindAccountByUsername(ctx, followerUsername)
+	if err != nil {
+		httperror.NotFound(w, err)
+		return
+	}
+
+	relationship := new(object.Relationship)
+	relationship.FollowingId = followingAccount.ID
+	relationship.FollowerId = followerAccount.ID
+
+	if err = repo.CreateFollowing(ctx, followingAccount.ID, followerAccount.ID); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(relationship); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
 }
