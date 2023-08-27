034de92200f24884a3b1346d8e016af500fb7f82
shima8823 <taiyaki1275@gmail.com>
2023-08-27 16:04:39 +0900

    refactor: fetchTimelineData関数をbuildQueryに変更

--- app/dao/status.go
+++ app/dao/status.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"strings"
 	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/domain/repository"
 
@@ -55,41 +54,10 @@ func (r *status) DeleteByID(ctx context.Context, id uint64) error {
 func (r *status) PublicTimeline(ctx context.Context, only_media, max_id, since_id, limit *uint64) ([]*object.Status, error) {
 	var entities []*object.Status
 
-	query, args := fetchTimelineData(since_id, max_id, limit)
+	query, args := buildQuery("status", "id", since_id, max_id, limit)
 	err := r.db.SelectContext(ctx, &entities, query, args...)
 	if err != nil {
 		return nil, err
 	}
 	return entities, nil
-}
-
-func fetchTimelineData(since_id, max_id, limit *uint64) (string, []interface{}) {
-	queryParts := []string{"SELECT * FROM status"}
-	var args []interface{}
-
-	if since_id != nil || max_id != nil {
-		conditions := []string{}
-
-		if since_id != nil {
-			conditions = append(conditions, "id >= ?")
-			args = append(args, *since_id)
-		}
-
-		if max_id != nil {
-			conditions = append(conditions, "id <= ?")
-			args = append(args, *max_id)
-		}
-
-		queryParts = append(queryParts, "WHERE "+strings.Join(conditions, " AND "))
-	}
-
-	queryParts = append(queryParts, "ORDER BY create_at DESC")
-
-	if limit != nil {
-		queryParts = append(queryParts, "LIMIT ?")
-		args = append(args, *limit)
-	}
-
-	query := strings.Join(queryParts, " ")
-	return query, args
 }
