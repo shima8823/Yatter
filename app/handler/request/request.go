fe74b97f823623860021358977944afb788e1730
shima8823 <taiyaki1275@gmail.com>
2023-08-27 21:54:06 +0900

    refactor: ParseQueryPointer, ParseLimitQueryを移動

--- app/handler/request/request.go
+++ app/handler/request/request.go
@@ -33,3 +33,55 @@ func UsernameOf(r *http.Request) (string, error) {
 	}
 	return username, nil
 }
+
+type (
+	parsedQuery struct {
+		id      uint64
+		isEmpty bool
+	}
+)
+
+const (
+	defaultLimit = 40
+	maxLimit     = 80
+)
+
+func ParseQueryPointer(s string) (*uint64, error) {
+	parsed, err := parseQuery(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if parsed.isEmpty {
+		return nil, nil
+	}
+	return &parsed.id, nil
+}
+
+func ParseLimitQuery(s string) (*uint64, error) {
+	parsed, err := parseQuery(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if parsed.isEmpty {
+		var limit uint64 = defaultLimit
+		return &limit, nil
+	} else if parsed.id > maxLimit {
+		var limit uint64 = maxLimit
+		return &limit, nil
+	}
+	return &parsed.id, nil
+}
+
+func parseQuery(s string) (parsedQuery, error) {
+	if s == "" {
+		return parsedQuery{isEmpty: true}, nil
+	}
+
+	if id, err := strconv.ParseUint(s, 10, 64); err != nil {
+		return parsedQuery{}, err
+	} else {
+		return parsedQuery{id: id}, nil
+	}
+}
