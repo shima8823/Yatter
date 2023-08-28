2ca325ec4ec41738661b402682c9fbf1375d5913
shima8823 <taiyaki1275@gmail.com>
2023-08-28 10:54:00 +0900

    refactor: queryをparseする部分を関数化

--- app/handler/timelines/get.go
+++ app/handler/timelines/get.go
@@ -11,25 +11,7 @@ import (
 func (h *handler) GetTimeline(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	only_media, err := request.ParseQueryPointer(r.URL.Query().Get("only_media"))
-	if err != nil {
-		httperror.BadRequest(w, err)
-		return
-	}
-
-	max_id, err := request.ParseQueryPointer(r.URL.Query().Get("max_id"))
-	if err != nil {
-		httperror.BadRequest(w, err)
-		return
-	}
-
-	since_id, err := request.ParseQueryPointer(r.URL.Query().Get("since_id"))
-	if err != nil {
-		httperror.BadRequest(w, err)
-		return
-	}
-
-	limit, err := request.ParseLimitQuery(r.URL.Query().Get("limit"))
+	only_media, max_id, since_id, limit, err := request.ParseQueries(r)
 	if err != nil {
 		httperror.BadRequest(w, err)
 		return
