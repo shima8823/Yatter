679e43bbab2ab6cbef672a6733798d72ca88f6db
shima8823 <taiyaki1275@gmail.com>
2023-08-30 11:48:04 +0900

    fix: 外部キー制約が違反されないようにする

---
+++ app/dao/relationship_test.go
@@ -0,0 +1,60 @@
+package dao_test
+
+import (
+	"context"
+	"testing"
+	"yatter-backend-go/app/domain/object"
+	"yatter-backend-go/app/domain/repository"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func setupRelationshipDAO(t *testing.T) (repository.Relationship, func()) {
+	dao := setupDAO(t)
+	dao.InitAll()
+	relationshipRepo := dao.Relationship()
+
+	cleanup := func() {
+		dao.InitAll()
+	}
+
+	return relationshipRepo, cleanup
+}
+
+func setupAccountDB(t *testing.T, ctx context.Context) {
+	dao := setupDAO(t)
+	dao.InitAll()
+	accountRepo := dao.Account()
+
+	accounts := []object.Account{
+		{
+			Username:     "test1",
+			PasswordHash: "password",
+		},
+		{
+			Username:     "test2",
+			PasswordHash: "password",
+		},
+	}
+
+	for _, account := range accounts {
+		err := accountRepo.CreateUser(ctx, &account)
+		assert.NoError(t, err)
+	}
+}
+
+func TestCreateFollowing(t *testing.T) {
+	repo, cleanup := setupRelationshipDAO(t)
+	defer cleanup()
+
+	ctx := context.Background()
+	setupAccountDB(t, ctx)
+
+	relationship := &object.Relationship{
+		FollowingId: 1,
+		FollowerId:  2,
+	}
+	var err error
+	err = repo.CreateFollowing(ctx, relationship.FollowingId, relationship.FollowerId)
+	assert.NoError(t, err)
+}
