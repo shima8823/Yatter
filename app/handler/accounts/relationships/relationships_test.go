8a7be52e12c1a5030c3f9cd7d73de3589a896303
shima8823 <taiyaki1275@gmail.com>
2023-09-03 11:36:10 +0900

    test: TestDeleteを追加

--- app/handler/accounts/relationships/relationships_test.go
+++ app/handler/accounts/relationships/relationships_test.go
@@ -105,6 +105,113 @@ func TestCreate(t *testing.T) {
 
 }
 
+func TestDelete(t *testing.T) {
+	db, mock := dao.NewMockDB()
+	h := newMockHandler(db)
+	defer db.Close()
+
+	tests := []struct {
+		name         string
+		mockFunc     func()
+		isAuth       bool
+		urlParamFunc func(r *http.Request) *http.Request
+		wantCode     int
+	}{
+		{
+			name: "successfully delete account",
+			mockFunc: func() {
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("followingUser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(1, "followingUser"))
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("unfollowUser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(2, "unfollowUser"))
+
+				mock.ExpectBegin()
+
+				mock.ExpectExec("delete from relationship where following_id = \\? and follower_id = \\?").
+					WithArgs(1, 2).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectExec("update account set following_count = following_count - 1 where id = \\?").
+					WithArgs(1).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectExec("update account set followers_count = followers_count - 1 where id = \\?").
+					WithArgs(2).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+
+				mock.ExpectCommit()
+			},
+			isAuth:       true,
+			urlParamFunc: func(r *http.Request) *http.Request { return setChiURLParam(r, "username", "unfollowUser") },
+			wantCode:     http.StatusOK,
+		},
+		{
+			name:     "Unauthorized",
+			wantCode: http.StatusUnauthorized,
+		},
+		{
+			name: "user not found",
+			mockFunc: func() {
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("followingUser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(1, "followingUser"))
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("unfollowUser").
+					WillReturnError(sql.ErrNoRows)
+			},
+			isAuth:       true,
+			urlParamFunc: func(r *http.Request) *http.Request { return setChiURLParam(r, "username", "unfollowUser") },
+			wantCode:     http.StatusNotFound,
+		},
+		{
+			name: "relationship not found",
+			mockFunc: func() {
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("followingUser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(1, "followingUser"))
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("unfollowUser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(2, "unfollowUser"))
+
+				mock.ExpectBegin()
+
+				mock.ExpectExec("delete from relationship where following_id = \\? and follower_id = \\?").
+					WithArgs(1, 2).
+					WillReturnResult(sqlmock.NewResult(0, 0))
+			},
+			isAuth:       true,
+			urlParamFunc: func(r *http.Request) *http.Request { return setChiURLParam(r, "username", "unfollowUser") },
+			wantCode:     http.StatusInternalServerError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, err := http.NewRequest(http.MethodDelete, "/v1/accounts/unfollowUser/follow", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if tt.urlParamFunc != nil {
+				r = tt.urlParamFunc(r)
+			}
+			if tt.isAuth {
+				r.Header.Set("Authentication", "username followingUser")
+			}
+			if tt.mockFunc != nil {
+				tt.mockFunc()
+			}
+
+			middleware := auth.Middleware(h.app)
+			handlerMiddleware := middleware(http.HandlerFunc(h.Delete))
+			handlerMiddleware.ServeHTTP(w, r)
+
+			assert.Equal(t, tt.wantCode, w.Code)
+		})
+	}
+
+}
+
 func newMockHandler(db *sql.DB) *handler {
 	return &handler{
 		app: &app.App{
