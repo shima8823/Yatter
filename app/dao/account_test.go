aa01cc7623640c1e7fedaca7e58d4b0621664b26
shima8823 <taiyaki1275@gmail.com>
2023-09-05 11:30:18 +0900

    refactor: dao_test.goにある変数を使用する

--- app/dao/account_test.go
+++ app/dao/account_test.go
@@ -2,67 +2,47 @@ package dao_test
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"yatter-backend-go/app/domain/object"
-	"yatter-backend-go/app/domain/repository"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func setupAccountDAO(t *testing.T) (repository.Account, func()) {
-	dao, err := setupDAO()
-	if err != nil {
-		t.Fatal(err)
-	}
-	dao.InitAll()
-	accountRepo := dao.Account()
-
-	cleanup := func() {
-		dao.InitAll()
-		dao.Close()
-	}
-
-	return accountRepo, cleanup
-}
-
 func TestAccountCreate(t *testing.T) {
-	repo, cleanup := setupAccountDAO(t)
-	defer cleanup()
 	ctx := context.Background()
+	cleanupDB()
 
 	t.Run("Success", func(t *testing.T) {
-		err := repo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
+		err := accountRepo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
 		assert.NoError(t, err)
 
-		createdAccount, err := repo.Retrieve(ctx, "testuser")
+		createdAccount, err := accountRepo.Retrieve(ctx, "testuser")
 		assert.NoError(t, err)
 		assert.NotNil(t, createdAccount)
 		assert.Equal(t, "testuser", createdAccount.Username)
 	})
 
 	t.Run("duplicate username", func(t *testing.T) {
-		err := repo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
+		err := accountRepo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
 		assert.Error(t, err)
 	})
 }
 
 func TestAccountRetrieve(t *testing.T) {
-	repo, cleanup := setupAccountDAO(t)
-	defer cleanup()
 	ctx := context.Background()
+	cleanupDB()
 
-	err := repo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
+	err := accountRepo.Create(ctx, &object.Account{Username: "testuser", PasswordHash: "hashedpassword"})
 	assert.NoError(t, err)
 
 	t.Run("Found", func(t *testing.T) {
-		account, err := repo.Retrieve(ctx, "testuser")
+		account, err := accountRepo.Retrieve(ctx, "testuser")
 		assert.NoError(t, err)
 		assert.NotNil(t, account)
 		assert.Equal(t, "testuser", account.Username)
 	})
 
 	t.Run("Not found", func(t *testing.T) {
-		account, err := repo.Retrieve(ctx, "nonexistentuser")
+		account, err := accountRepo.Retrieve(ctx, "nonexistentuser")
 		assert.Error(t, err)
 		assert.Nil(t, account)
 	})
--- app/dao/status_test.go
+++ app/dao/status_test.go
@@ -2,105 +2,81 @@ package dao_test
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 	"yatter-backend-go/app/domain/object"
-	"yatter-backend-go/app/domain/repository"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func setupStatusDAO(t *testing.T) (repository.Status, func()) {
-	dao, err := setupDAO()
-	if err != nil {
-		t.Fatal(err)
-	}
-	dao.InitAll()
-	statusRepo := dao.Status()
-
-	cleanup := func() {
-		dao.InitAll()
-	}
-
-	return statusRepo, cleanup
-}
-
 func TestStatusCreate(t *testing.T) {
-	repo, cleanup := setupStatusDAO(t)
-	defer cleanup()
-
 	ctx := context.Background()
+	cleanupDB()
+
 	status := &object.Status{
 		AccountId: 1,
 		Content:   "Test Content",
 	}
-	err := repo.Create(ctx, status)
+	err := statusRepo.Create(ctx, status)
 	assert.NoError(t, err)
 
-	createdStatus, err := repo.Retrieve(ctx, 1)
+	createdStatus, err := statusRepo.Retrieve(ctx, 1)
 	assert.NoError(t, err)
 	assert.NotNil(t, createdStatus)
 }
 
 func TestStatusRetrieve(t *testing.T) {
-	repo, cleanup := setupStatusDAO(t)
-	defer cleanup()
-
 	ctx := context.Background()
+	cleanupDB()
 
 	status := &object.Status{
 		AccountId: 1,
 		Content:   "Test Content for Find",
 	}
-	err := repo.Create(ctx, status)
+	err := statusRepo.Create(ctx, status)
 	assert.NoError(t, err)
 
 	t.Run("Found", func(t *testing.T) {
-		foundStatus, err := repo.Retrieve(ctx, 1)
+		foundStatus, err := statusRepo.Retrieve(ctx, 1)
 		assert.NoError(t, err)
 		assert.NotNil(t, foundStatus)
 		assert.Equal(t, status.Content, foundStatus.Content)
 	})
 
 	t.Run("NotFound", func(t *testing.T) {
-		foundStatus, err := repo.Retrieve(ctx, 2)
+		foundStatus, err := statusRepo.Retrieve(ctx, 2)
 		assert.NoError(t, err)
 		assert.Nil(t, foundStatus)
 	})
 }
 
 func TestStatusDelete(t *testing.T) {
-	repo, cleanup := setupStatusDAO(t)
-	defer cleanup()
-
 	ctx := context.Background()
+	cleanupDB()
 
 	status := &object.Status{
 		AccountId: 1,
 		Content:   "Test Content for Delete",
 	}
-	err := repo.Create(ctx, status)
+	err := statusRepo.Create(ctx, status)
 	assert.NoError(t, err)
 
 	t.Run("Delete", func(t *testing.T) {
-		err = repo.Delete(ctx, 1)
+		err = statusRepo.Delete(ctx, 1)
 		assert.NoError(t, err)
-		deletedStatus, err := repo.Retrieve(ctx, 1)
+		deletedStatus, err := statusRepo.Retrieve(ctx, 1)
 		assert.NoError(t, err)
 		assert.Nil(t, deletedStatus)
 	})
 
 	t.Run("DeleteNotFound", func(t *testing.T) {
-		err = repo.Delete(ctx, 2)
+		err = statusRepo.Delete(ctx, 2)
 		assert.NoError(t, err)
 	})
 }
 
 func TestPublicTimeline(t *testing.T) {
-	repo, cleanup := setupStatusDAO(t)
-	defer cleanup()
-
 	ctx := context.Background()
+	cleanupDB()
 
 	// テストのためのデータを作成
 	for i := 1; i <= 10; i++ {
@@ -108,33 +84,33 @@ func TestPublicTimeline(t *testing.T) {
 			AccountId: uint64(i),
 			Content:   "Test Content " + strings.Repeat("#", i),
 		}
-		err := repo.Create(ctx, status)
+		err := statusRepo.Create(ctx, status)
 		assert.NoError(t, err)
 	}
 
 	t.Run("All", func(t *testing.T) {
-		allStatuses, err := repo.PublicTimeline(ctx, nil, nil, nil, nil)
+		allStatuses, err := statusRepo.PublicTimeline(ctx, nil, nil, nil, nil)
 		assert.NoError(t, err)
 		assert.Equal(t, 10, len(allStatuses))
 	})
 
 	t.Run("Limit", func(t *testing.T) {
 		limit := uint64(5)
-		limitedStatuses, err := repo.PublicTimeline(ctx, nil, nil, nil, &limit)
+		limitedStatuses, err := statusRepo.PublicTimeline(ctx, nil, nil, nil, &limit)
 		assert.NoError(t, err)
 		assert.Equal(t, 5, len(limitedStatuses))
 	})
 
 	t.Run("SinceID", func(t *testing.T) {
 		sinceID := uint64(5)
-		sinceIDStatuses, err := repo.PublicTimeline(ctx, nil, nil, &sinceID, nil)
+		sinceIDStatuses, err := statusRepo.PublicTimeline(ctx, nil, nil, &sinceID, nil)
 		assert.NoError(t, err)
 		assert.Equal(t, 6, len(sinceIDStatuses))
 	})
 
 	t.Run("MaxID", func(t *testing.T) {
 		maxID := uint64(5)
-		maxIDStatuses, err := repo.PublicTimeline(ctx, nil, &maxID, nil, nil)
+		maxIDStatuses, err := statusRepo.PublicTimeline(ctx, nil, &maxID, nil, nil)
 		assert.NoError(t, err)
 		assert.Equal(t, 5, len(maxIDStatuses))
 	})
@@ -142,7 +118,7 @@ func TestPublicTimeline(t *testing.T) {
 	t.Run("SinceIDAndMaxID", func(t *testing.T) {
 		sinceID := uint64(5)
 		maxID := uint64(8)
-		sinceIDAndMaxIDStatuses, err := repo.PublicTimeline(ctx, nil, &maxID, &sinceID, nil)
+		sinceIDAndMaxIDStatuses, err := statusRepo.PublicTimeline(ctx, nil, &maxID, &sinceID, nil)
 		assert.NoError(t, err)
 		assert.Equal(t, 4, len(sinceIDAndMaxIDStatuses))
 	})
