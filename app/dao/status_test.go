ca30fd1bf4d57d1a7c468e26e00901a9477ffee9
shima8823 <taiyaki1275@gmail.com>
2023-09-06 12:21:46 +0900

    test: TestHomeTimelineを追加

--- app/dao/status_test.go
+++ app/dao/status_test.go
@@ -186,6 +186,84 @@ func TestPublicTimeline(t *testing.T) {
 	}
 }
 
+func TestHomeTimeline(t *testing.T) {
+	ctx := context.Background()
+	cleanupDB()
+	defer cleanupDB()
+	followingID := uint64(1)
+	insertRelationshipDB(t, ctx, []object.Relationship{
+		{
+			FollowingId: followingID,
+			FollowerId:  2,
+		},
+	})
+	for i := 1; i <= 10; i++ {
+		status := &object.Status{
+			AccountId: 2,
+			Content:   "Test Content " + strings.Repeat("#", i),
+		}
+		err := statusRepo.Create(ctx, status)
+		assert.NoError(t, err)
+	}
+
+	tests := []struct {
+		name      string
+		accountId uint64
+		expectLen int
+		wantArgs  func() (only_media, max_id, since_id, limit *uint64)
+	}{
+		{
+			name:      "All",
+			accountId: followingID,
+			expectLen: 10,
+			wantArgs: func() (only_media, max_id, since_id, limit *uint64) {
+				return nil, nil, nil, nil
+			},
+		},
+		{
+			name:      "Limit",
+			accountId: followingID,
+			expectLen: 5,
+			wantArgs: func() (only_media, max_id, since_id, limit *uint64) {
+				return nil, nil, nil, newUint64(5)
+			},
+		},
+		{
+			name:      "SinceID",
+			accountId: followingID,
+			expectLen: 6,
+			wantArgs: func() (only_media, max_id, since_id, limit *uint64) {
+				return nil, nil, newUint64(5), nil
+			},
+		},
+		{
+			name:      "MaxID",
+			accountId: followingID,
+			expectLen: 5,
+			wantArgs: func() (only_media, max_id, since_id, limit *uint64) {
+				return nil, newUint64(5), nil, nil
+			},
+		},
+		{
+			name:      "SinceIDAndMaxID",
+			accountId: followingID,
+			expectLen: 4,
+			wantArgs: func() (only_media, max_id, since_id, limit *uint64) {
+				return nil, newUint64(8), newUint64(5), nil
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			only_media, max_id, since_id, limit := tt.wantArgs()
+			allStatuses, err := statusRepo.HomeTimeline(ctx, tt.accountId, only_media, max_id, since_id, limit)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expectLen, len(allStatuses))
+		})
+	}
+}
+
 func newUint64(i uint64) *uint64 {
 	return &i
 }
