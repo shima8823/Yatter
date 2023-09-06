b124c6e3c4261b0d9e5bcb34f4bcb052aed0cb9f
shima8823 <taiyaki1275@gmail.com>
2023-09-06 14:05:27 +0900

    test: TestGetHomeを追加

--- app/handler/timelines/timelines_test.go
+++ app/handler/timelines/timelines_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"yatter-backend-go/app/app"
 	"yatter-backend-go/app/dao"
+	"yatter-backend-go/app/handler/auth"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi"
@@ -62,7 +63,78 @@ func TestGetPublic(t *testing.T) {
 				tt.mockFunc()
 			}
 			h.GetPublic(w, r)
-			assert.Equal(t, tt.wantCode, w.Code, "status code should be equal")
+			assert.Equal(t, tt.wantCode, w.Code)
+		})
+	}
+}
+
+func TestGetHome(t *testing.T) {
+	db, mock := dao.NewMockDB()
+	h := newMockHandler(db)
+	defer db.Close()
+
+	tests := []struct {
+		name     string
+		username string
+		mockFunc func()
+		isAuth   bool
+		wantCode int
+	}{
+		{
+			name:     "Success",
+			username: "testuser",
+			mockFunc: func() {
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("testuser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(1, "testuser"))
+				mock.ExpectQuery("select status.\\* from status join relationship on status.account_id = relationship.follower_id where relationship.following_id = \\? order by status.create_at desc limit \\?").
+					WithArgs(1, 40).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "account_id", "content"}).
+						AddRow(1, 1, "test content").
+						AddRow(2, 1, "test content2"))
+			},
+			isAuth:   true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name: "no timeline",
+			mockFunc: func() {
+				mock.ExpectQuery("select \\* from account where username = \\?").
+					WithArgs("testuser").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "username"}).AddRow(1, "testuser"))
+				mock.ExpectQuery("select status.\\* from status join relationship on status.account_id = relationship.follower_id where relationship.following_id = \\? order by status.create_at desc limit \\?").
+					WithArgs(1, 40).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "account_id", "content"}))
+			},
+			isAuth:   true,
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "unauthorized",
+			wantCode: http.StatusUnauthorized,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, err := http.NewRequest(http.MethodGet, "/v1/timelines/home", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tt.isAuth {
+				r.Header.Set("Authentication", "username testuser")
+			}
+
+			if tt.mockFunc != nil {
+				tt.mockFunc()
+			}
+
+			middleware := auth.Middleware(h.app)
+			handlerMiddleware := middleware(http.HandlerFunc(h.GetHome))
+			handlerMiddleware.ServeHTTP(w, r)
+			assert.Equal(t, tt.wantCode, w.Code)
 		})
 	}
 }
