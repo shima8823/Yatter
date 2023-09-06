bd54c2e5ae985c84f5b8cdec0823cc3ae3abad2a
shima8823 <taiyaki1275@gmail.com>
2023-09-06 10:58:38 +0900

    fix: dbがErrNoRowsの場合NotFoundを返す

--- app/handler/statuses/get.go
+++ app/handler/statuses/get.go
@@ -1,6 +1,7 @@
 package statuses
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"yatter-backend-go/app/handler/httperror"
@@ -19,6 +20,10 @@ func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	if objAccount, err := h.app.Dao.Status().Retrieve(ctx, id); err != nil {
+		if err == sql.ErrNoRows {
+			httperror.NotFound(w, id)
+			return
+		}
 		httperror.InternalServerError(w, err)
 		return
 	} else if objAccount != nil {
